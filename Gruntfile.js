22a0dd1419444e9942b26bf8a0409d93dd25edda
FrankFan <fanyong@gmail.com>
2015-02-23 10:21:51 +0800

    use grunt-contrib-concat to concat files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,88 +1,101 @@
 'use strict';
 
-module.exports = function (grunt) {
-	require('load-grunt-tasks')(grunt);
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
 
-	require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 
-	var config = {
-		app: 'app',
-		dist: 'dist'
-	}
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  }
 
-	grunt.initConfig({
-		config: config,
+  grunt.initConfig({
+    config: config,
 
-		copy: {
-			dist_html: {
-				// src 和 dest 键值对形式
-				// files: [
-				// 	{
-				// 		src: '<%= config.app %>/index.html',
-				// 		dest: '<%= config.dist %>/index.html'
-				// 	},
-				// 	{
-				// 		src: '<%= config.app %>/js/index.js',
-				// 		dest: '<%= config.dist %>/js/index.js'
-				// 	}
-				// ],
+    copy: {
+      dist_html: {
+        // src 和 dest 键值对形式
+        // files: [
+        //  {
+        //      src: '<%= config.app %>/index.html',
+        //      dest: '<%= config.dist %>/index.html'
+        //  },
+        //  {
+        //      src: '<%= config.app %>/js/index.js',
+        //      dest: '<%= config.dist %>/js/index.js'
+        //  }
+        // ],
 
-				// file object format 形式， key是dest，value是src
-				// value可以是string、array等任意类型
-				// files: {
-				// 	'<%= config.dist %>/index.html': '<%= config.app %>/index.html',
-				// 	'<%= config.dist %>/js/index.js': ['<%= config.app %>/js/index.js']
-				// }
+        // file object format 形式， key是dest，value是src
+        // value可以是string、array等任意类型
+        // files: {
+        //  '<%= config.dist %>/index.html': '<%= config.app %>/index.html',
+        //  '<%= config.dist %>/js/index.js': ['<%= config.app %>/js/index.js']
+        // }
 
-				files: [
-					{
-						expand: true,
-						cwd: '<%= config.app %>/',
-						src: '**/*.js', //'*.html',
-						dest: '<%= config.dist %>/',
-						ext: '.js', //'.min.html',
-						// extDot: 'first' | 'last'
-						flattern: true, // 去掉各层目录
-						rename: function (dest, src) {
-							return dest + src;
-						}
-					}
-				]
-				
-			},
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/',
+          src: '**/*.js', //'*.html',
+          dest: '<%= config.dist %>/',
+          ext: '.js', //'.min.html',
+          // extDot: 'first' | 'last'
+          flattern: true, // 去掉各层目录
+          rename: function(dest, src) {
+            return dest + src;
+          }
+        }]
 
-			// dist_js: {
-			// 	src: '<%= config.app %>/js/index.js',
-			// 	dest: '<%= config.dist %>/js/index.js'
-			// }
-		},
+      },
 
-		clean: {
-			dist: {
-				// 1. 单独指定文件名
-				// src: ['<%= config.dist %>/index.html',
-				//  '<%= config.dist %>/js/index.js']
+      // dist_js: {
+      //  src: '<%= config.app %>/js/index.js',
+      //  dest: '<%= config.dist %>/js/index.js'
+      // }
+    },
 
-				// 2. 通配符
-				// *：匹配任意字符，但是不匹配斜杠/
-				// **：匹配任意数量的任意字符，包括斜杠
-				// ?：只匹配一个字符，除了斜杠/
-				// {a, b}.js: a.js or b.js
-				// !: 取反
-				src: ['<%= config.dist %>/**/*'],
+    clean: {
+      dist: {
+        // 1. 单独指定文件名
+        // src: ['<%= config.dist %>/index.html',
+        //  '<%= config.dist %>/js/index.js']
 
-				// filter: 'isFile' 只删除文件，保留目录
+        // 2. 通配符
+        // *：匹配任意字符，但是不匹配斜杠/
+        // **：匹配任意数量的任意字符，包括斜杠
+        // ?：只匹配一个字符，除了斜杠/
+        // {a, b}.js: a.js or b.js
+        // !: 取反
+        src: ['<%= config.dist %>/**/*'],
 
-				// 自定义filter函数
-				filter: function (filepath) {
-					return (!grunt.file.isDir(filepath));
-				},
-				//nonull
-				// dot: true, .gitignore
-				// matchBase: 匹配路径的baseName
-				// expand: true; 处理动态的文件src到dest映射
-			}
-		}
+        // filter: 'isFile' 只删除文件，保留目录
 
-	});
+        // 自定义filter函数
+        filter: function(filepath) {
+          return (!grunt.file.isDir(filepath));
+        },
+        //nonull
+        // dot: true, .gitignore
+        // matchBase: 匹配路径的baseName
+        // expand: true; 处理动态的文件src到dest映射
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: [
+          '<%= config.app %>/scripts/index.js',
+          '<%= config.app %>/scripts/main1.js',
+          '<%= config.app %>/scripts/main2.js',
+          '<%= config.app %>/scripts/main3.js'
+        ],
+        dest: '<%= config.dist %>/scripts/concated.js'
+      }
+    }
+
+  });
 }
