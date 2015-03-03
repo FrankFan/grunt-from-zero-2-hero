21de0787e8b1744a3697f48a09feb2d76c16f347
fanyong <y.fan@Ctrip.com>
2015-03-03 18:16:37 +0800

    config cssmin grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -145,6 +145,26 @@ module.exports = function(grunt) {
       }
     },
 
+    cssmin: {
+      minify: {
+        expand: true, // 如果设为true，就表示下面文件名的占位符（即*号）都要扩展成具体的文件名
+        cwd: '<%= config.app %>/styles/', // 需要处理的文件（input）所在的目录
+        src: [ // // 表示需要处理的文件。如果采用数组形式，数组的每一项就是一个文件名，可以使用通配符
+              '*.css',
+              '!*.min.css'
+            ],
+        dest: '<%= config.dist %>/styles/', // 表示处理后的文件名或所在目录(output)
+        ext: '.min.css' // 表示处理后的文件后缀名
+      },
+      combine: {
+        files: {
+          '<%= config.dist %>/styles/main.min.css': [
+            '<%= config.app %>/styles/*.css'
+          ]
+        }
+      }
+    },
+
     // 监听文件变化
     watch: {
       js: {
